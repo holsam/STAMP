8aebc557be27efc5abca6303c83ae21a64fb338c
Sam Holland <hol_sam@icloud.com>
2026-09-02 16:50:51 +0100

    test(classify): add tests for classify command
    - Added tests/unit/tests_classify.py to define unit tests for extraction,
    features and clustering.
    - Added tests/integration/test_classify_command.py to define integration
    tests for classify command.
    - Modified tests/unit/test_cli.py to remove classify command from
    expected stubs.

--- tests/unit/test_cli.py
+++ tests/unit/test_cli.py
@@ -13,7 +13,7 @@ from stamp.cli.cli import stamp
 runner = CliRunner()
 
 # Define constants
-STUB_COMMANDS = ['classify', 'identify', 'refine', 'pipeline']
+STUB_COMMANDS = ['identify', 'refine', 'pipeline']
 
 # TestCli: class containing CLI unit tests
 class TestCli:
