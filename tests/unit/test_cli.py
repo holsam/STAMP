b04e684e3e54e00d47e5a9b64127ebd287c8494c
Sam Holland <hol_sam@icloud.com>
2026-09-03 08:47:04 +0100

    test(identify): add tests for identify command
    - Added tests/unit/tests_identify.py to define unit tests for protein
    candidate parsing, density simulation, fitting and decoy checking.
    - Added tests/integration/test_identify_command.py to define integration
    tests for identify command.
    - Modified tests/unit/test_cli.py to remove identify command from
    expected stubs.

--- tests/unit/test_cli.py
+++ tests/unit/test_cli.py
@@ -13,7 +13,7 @@ from stamp.cli.cli import stamp
 runner = CliRunner()
 
 # Define constants
-STUB_COMMANDS = ['identify', 'refine', 'pipeline']
+STUB_COMMANDS = ['refine', 'pipeline']
 
 # TestCli: class containing CLI unit tests
 class TestCli:
