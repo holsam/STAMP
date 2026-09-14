fcfe2f9f58b51d0deab16bcb3c9e1f9ada53f3c3
Sam Holland <hol_sam@icloud.com>
2026-09-14 09:23:00 +0100

    feat(cli): configure logging
    - Modified cli/cli.py to configure logging via callback when STAMP runs.

--- src/stamp/cli/cli.py
+++ src/stamp/cli/cli.py
@@ -3,10 +3,13 @@ STAMP: CLI entrypoint
 '''
 
 # Import external dependencies
-import typer
+import sys, typer
+from pathlib import Path
+from typing import Annotated, Literal
 
 # Import internal commands
 from stamp.cli import classify, decoy, identify, pick, refine, run, tools
+from stamp.utils.log import configure_logging, log
 
 # Initialise Typer app 
 stamp = typer.Typer(
@@ -14,6 +17,7 @@ stamp = typer.Typer(
     help = 'Sub-Tomogram Averaging Membrane Protein pipeline',
     no_args_is_help = True,
     add_completion = False,
+    context_settings={'help_option_names': ['-h', '--help']},
 )
 
 # Add command Typer classes
@@ -29,3 +33,37 @@ stamp.add_typer(
     help='Misc STAMP tools.',
     rich_help_panel='Utilities'
 )
+
+# logging_callback: provide logging options and configure logging
+@stamp.callback()
+def logging_callback(
+    ctx: typer.Context,
+    log_dir: Annotated[
+        Path | None,
+        typer.Option('-d', '--directory', help='Directory to write log file to.', show_default=None, rich_help_panel='Logging Options')
+    ] = None,
+    log_mode: Annotated[
+        Literal['append', 'overwrite'],
+        typer.Option('-m', '--mode', help='Mode for handling existing log files.', rich_help_panel='Logging Options')
+    ] = 'append',
+    quiet: Annotated[
+        int,
+        typer.Option('-q', '--quiet', count=True, help='Decrease logging verbosity.', show_default=False, rich_help_panel='Logging Options', metavar='')
+    ] = 0,
+    verbosity: Annotated[
+        int,
+        typer.Option('-v', '--verbose', count=True, help='Increase logging verbosity.', show_default=False, rich_help_panel='Logging Options', metavar='')
+    ] = 0,
+) -> None:
+    if ctx.resilient_parsing or ctx.invoked_subcommand is None:
+        return
+    if {'--help', '-h'} & set(sys.argv[1:]):
+        return
+    if quiet and verbosity:
+        raise typer.BadParameter('-q/--quiet and -v/--verbose are mutually exclusive.')
+    verbosity = min(verbosity, 2)
+    quiet = min(quiet, 2)
+    log_path, log_level = configure_logging(directory=log_dir, mode=log_mode, quiet=quiet, verbosity=verbosity)
+    print()
+    log.debug(f'Logging configured to stderr and log file (mode: {log_mode}) at level {log_level}')
+    log.info(f'Logs will be saved to: {log_path}')
